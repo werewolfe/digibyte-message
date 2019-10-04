3180a295b9bd005890365acb2e63acaea7596d3a
Adam Wolfe <awolfe@live.com>
2019-10-04 16:52:28 -0700

    update packages

---
+++ node_modules/sffc-encoder/sffcEncoder.js
@@ -0,0 +1,111 @@
+var flagMask = 0xe0
+var encodingSchemeTable =
+[
+  {
+    'flag': 0x20,
+    'exponent': 4,
+    'byteSize': 2,
+    'mantis': 9
+  },
+  {
+    'flag': 0x40,
+    'exponent': 4,
+    'byteSize': 3,
+    'mantis': 17
+  },
+  {
+    'flag': 0x60,
+    'exponent': 4,
+    'byteSize': 4,
+    'mantis': 25
+  },
+  {
+    'flag': 0x80,
+    'exponent': 3,
+    'byteSize': 5,
+    'mantis': 34
+  },
+  {
+    'flag': 0xa0,
+    'exponent': 3,
+    'byteSize': 6,
+    'mantis': 42
+  },
+  {
+    'flag': 0xc0,
+    'exponent': 0,
+    'byteSize': 7,
+    'mantis': 54
+  }
+]
+
+var flagLookup = {}
+var mantisLookup = {}
+
+for (var i = 0; i < encodingSchemeTable.length; i++) {
+  var flagObject = encodingSchemeTable[i]
+  flagLookup[flagObject.flag] = flagObject
+}
+var currentIndex = 0
+var currentMantis = encodingSchemeTable[currentIndex].mantis
+var endMantis = encodingSchemeTable[encodingSchemeTable.length - 1].mantis
+
+for (var i = 1; i <= endMantis; i++) {
+  if (i > currentMantis) {
+    currentIndex++
+    currentMantis = encodingSchemeTable[currentIndex].mantis
+  }
+  mantisLookup[i] = encodingSchemeTable[currentIndex]
+}
+
+var intToFloatArray = function (number, n) {
+  n = n || 0
+  return number % 10 ? [number, n] : intToFloatArray(number / 10, n + 1)
+}
+
+var padLeadingZeros = function (hex, byteSize) {
+  return (hex.length === byteSize * 2) ? hex : padLeadingZeros('0' + hex, byteSize)
+}
+
+module.exports = {
+  encode: function (number) {
+    var buf
+    if (number < 0) throw new Error('Number is out of bounds')
+    if (number > Number.MAX_SAFE_INTEGER) throw new Error('Number is out of bounds')
+    if (number < 32) {
+      buf = new Buffer([number])
+      return buf
+    }
+    var floatingNumberArray = intToFloatArray(number)
+    while (true) {
+      var encodingObject = mantisLookup[floatingNumberArray[0].toString(2).length]
+      if (!encodingObject) throw new Error('Number is out of bounds')
+      if ((Math.pow(2, encodingObject.exponent) - 1) >= floatingNumberArray[1]) break
+      floatingNumberArray[0] *= 10
+      floatingNumberArray[1] -= 1
+    }
+    var shiftedNumber = floatingNumberArray[0] * Math.pow(2, encodingObject.exponent)
+    var numberString = padLeadingZeros(shiftedNumber.toString(16), encodingObject.byteSize)
+    buf = new Buffer(numberString, 'hex')
+    buf[0] = buf[0] | encodingObject.flag
+    buf[buf.length - 1] = buf[buf.length - 1] | floatingNumberArray[1]
+
+    return buf
+  },
+
+  decode: function (consume) {
+    var flagByte = consume(1)[0]
+    var flag = flagByte & flagMask
+    if (flag === 0) return flagByte
+    if (flag === 0xe0) flag = 0xc0
+    var encodingObject = flagLookup[flag]
+    var headOfNumber = new Buffer([flagByte & (~flag)])
+    var tailOfNumber = consume(encodingObject.byteSize - 1)
+    var fullNumber = Buffer.concat([headOfNumber, tailOfNumber])
+    var number = parseInt(fullNumber.toString('hex'), 16)
+    var exponentShift = Math.pow(2, encodingObject.exponent)
+    var exponent = number % exponentShift
+    var mantis = Math.floor(number / exponentShift)
+    return mantis * Math.pow(10, exponent)
+  }
+}
---
+++ node_modules/sffc-encoder/test/test.js
@@ -0,0 +1,91 @@
+var balz = require(__dirname + '/../sffcEncoder')
+var assert = require('assert')
+var fullRunLength = 1000000
+var maxNumber = 9007199254740991
+var samples = [
+  1,
+  1200032,
+  1232,
+  1002000000,
+  928867423145164,
+  132300400000,
+  1323004030000,
+  32300400400,
+  2300400002,
+  100000000000,
+  100001000000,
+  110000000010000,
+  1768474864449384,
+  7684748644493848,
+  1111111111111111,
+  1222222222222222,
+  1333333333333333,
+  3333333333232323,
+  2343241324231432,
+  9007199254740991,
+  4823750656226800
+]
+
+var consumer = function (buff) {
+  var curr = 0
+  return function consume (len) {
+    return buff.slice(curr, curr += len)
+  }
+}
+
+describe('One byte encoding', function () {
+  it('should return the right encoding for numbers 0-31', function (done) {
+    this.timeout(0)
+    var labz_code
+    for (var i = 0; i < 32; i++) {
+      labz_code = balz.encode(i)
+      var buf = new Buffer([i])
+      assert.equal(labz_code.length, 1, 'Wrong encoding length')
+      assert.equal(labz_code.toString('hex'), buf.toString('hex'), 'Wrong encoding')
+    }
+    done()
+  })
+})
+
+describe('Encode/Decode', function (done) {
+  it('should decode encoded numbers correctly for all numbers between 0 to ' + fullRunLength, function (done) {
+    this.timeout(0)
+    for (var i = 0; i < fullRunLength; i++) {
+      var labz_code = balz.encode(i)
+      assert.equal(balz.decode(consumer(labz_code)), i, 'Wrong encode/decode fullRunLength')
+    }
+    done()
+  })
+  it('should decode encoded numbers correctly for sample run', function (done) {
+    this.timeout(0)
+    for (var i = 0; i < samples.length; i++) {
+      var labz_code = balz.encode(samples[i])
+      assert.equal(balz.decode(consumer(labz_code)), samples[i], 'Wrong encode/decode sample')
+    }
+    done()
+  })
+  it('should decode encoded numbers correctly for ' + fullRunLength + ' random numbers in the allowed range', function (done) {
+    this.timeout(0)
+    for (var i = 0; i < fullRunLength; i++) {
+      var number = Math.floor(Math.random() * maxNumber) + 1
+      var labz_code = balz.encode(number)
+      assert.equal(balz.decode(consumer(labz_code)), number, 'Wrong encode/decode random')
+    }
+    done()
+  })
+})
+
+describe('Should return errors', function (done) {
+  it('Should throw errors', function (done) {
+    this.timeout(0)
+    samples = [9007199254740992, 10000000000000000000, -132, -1231]
+    for (var i = 0; i < samples.length; i++) {
+      assert.throws(function () {
+        balz.encode(samples[i])
+      }
+      , 'Number is out of bounds'
+      , 'Out of bound number should throw new error')
+    }
+    done()
+  })
+})
